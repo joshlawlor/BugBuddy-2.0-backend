7dc86a7bfdd88212a656820943ff28d82cbc4906
qjoshlawlor <joshlawlor1@gmail.com>
2022-11-21 18:39:49 -0500

    Connected to fresh Database on mongoDB. Models are next

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
-const PORT = 40000;
+const cors = require('cors')
+
+const PORT = 4000;
+
+require('./config/database')
+app.use(cors({origin: '*', methods: "GET, POST, PUT, DELETE, PATCH", credentials: true}))
+
+//MIDDLEWARES
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// Need auth for token later on
+
 
 
 app.listen(PORT, () => {
