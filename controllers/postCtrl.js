a8b354e8e2c57c10b1b3b56dbc3778aa1fce1562
qjoshlawlor <joshlawlor1@gmail.com>
2022-12-07 19:21:52 -0500

    Added delete/update routes for posts

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -50,10 +50,68 @@ const create = (req,res) =>{
 
 
 }
+const deletePost = (req,res) => {
+    console.log('deleted')
+    Post.findByIdAndDelete({_id: req.params.id}, (err,post) => {
+        if(err) {
+            res.status(400).json(err)
+        }
+        res.json({msg: 'Post Deleted'})
+    })
+    User.findOne({email: req.user.email}, (err, user) => {
+        if(err){
+            res.status(400).json(err)
+        }
+        Post.findById({_id: req.params.id}, (err,post) => {
+            user.posts.pop(post)
+            user.save()
+        })
+    })
+
+}
+
+const updatePost = (req,res) => {
+    Post.findByIdAndUpdate(req.params.id, req.body, (err, post) =>{
+        if(err){
+            res.status(400).json()
+            return
+        }
+        res.json(post)
+    })   
+ 
+    User.findOne({ email: req.user.email }, (err,user) => {
+        if(err){
+            res.status(400).json(err)
+        }
+        user.posts.map((post) => {
+            const id = post._id.toString()
+            if(id == req.params.id){
+                console.log('BEFORE UPDATE', post.title, post.content)
+                post.title = req.body.title
+                post.content = req.body.content
+
+                console.log(`POST:`,user.posts)
+                console.log('AFTER UPDATE', post.title, post.content)
+                user.save()
+
+
+            }else{
+                console.log(post._id)
+                console.log(req.params.id)
+            }
+
+        })  
+       
+
+
+    })
+}
 
 
 module.exports = {
     showAll,
     create,
-    showOne
+    showOne,
+    updatePost,
+    deletePost
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,6 +5,8 @@ const postCtrl = require('../controllers/postCtrl')
 
 router.get('/', postCtrl.showAll)
 router.get('/:id', postCtrl.showOne)
+router.put('/:id/edit', postCtrl.updatePost)
+router.delete('/:id', postCtrl.deletePost)
 router.post('/', postCtrl.create)
 
 
